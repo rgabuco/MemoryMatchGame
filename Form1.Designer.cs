b1cbeddad26b82a62aeab099b7dd18d3b9cd5dc1
Rudy Gabuco Jr <rudyjr.gabuco@gmail.com>
2024-10-11 17:17:57 -0600

    initial source code commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace MemoryMatchGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            label1 = new Label();
+            btnMode = new Button();
+            btnRestart = new Button();
+            lblScore = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 100);
+            button1.Name = "button1";
+            button1.Size = new Size(38, 34);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += Card_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(83, 100);
+            button2.Name = "button2";
+            button2.Size = new Size(38, 34);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += Card_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(127, 100);
+            button3.Name = "button3";
+            button3.Size = new Size(38, 34);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += Card_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(171, 100);
+            button4.Name = "button4";
+            button4.Size = new Size(38, 34);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += Card_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(215, 100);
+            button5.Name = "button5";
+            button5.Size = new Size(38, 34);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += Card_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(39, 140);
+            button6.Name = "button6";
+            button6.Size = new Size(38, 34);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += Card_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(83, 140);
+            button7.Name = "button7";
+            button7.Size = new Size(38, 34);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += Card_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(127, 140);
+            button8.Name = "button8";
+            button8.Size = new Size(38, 34);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += Card_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(171, 140);
+            button9.Name = "button9";
+            button9.Size = new Size(38, 34);
+            button9.TabIndex = 8;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += Card_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(215, 140);
+            button10.Name = "button10";
+            button10.Size = new Size(38, 34);
+            button10.TabIndex = 9;
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += Card_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(39, 180);
+            button11.Name = "button11";
+            button11.Size = new Size(38, 34);
+            button11.TabIndex = 10;
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += Card_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(83, 180);
+            button12.Name = "button12";
+            button12.Size = new Size(38, 34);
+            button12.TabIndex = 11;
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += Card_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(127, 180);
+            button13.Name = "button13";
+            button13.Size = new Size(38, 34);
+            button13.TabIndex = 12;
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += Card_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(171, 180);
+            button14.Name = "button14";
+            button14.Size = new Size(38, 34);
+            button14.TabIndex = 13;
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += Card_Click;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(215, 180);
+            button15.Name = "button15";
+            button15.Size = new Size(38, 34);
+            button15.TabIndex = 14;
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += Card_Click;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(39, 220);
+            button16.Name = "button16";
+            button16.Size = new Size(38, 34);
+            button16.TabIndex = 15;
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += Card_Click;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(83, 220);
+            button17.Name = "button17";
+            button17.Size = new Size(38, 34);
+            button17.TabIndex = 16;
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += Card_Click;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(127, 220);
+            button18.Name = "button18";
+            button18.Size = new Size(38, 34);
+            button18.TabIndex = 17;
+            button18.UseVisualStyleBackColor = true;
+            button18.Click += Card_Click;
+            // 
+            // button19
+            // 
+            button19.Location = new Point(171, 220);
+            button19.Name = "button19";
+            button19.Size = new Size(38, 34);
+            button19.TabIndex = 18;
+            button19.UseVisualStyleBackColor = true;
+            button19.Click += Card_Click;
+            // 
+            // button20
+            // 
+            button20.Location = new Point(215, 220);
+            button20.Name = "button20";
+            button20.Size = new Size(38, 34);
+            button20.TabIndex = 19;
+            button20.UseVisualStyleBackColor = true;
+            button20.Click += Card_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label1.Location = new Point(64, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(162, 20);
+            label1.TabIndex = 20;
+            label1.Text = "Memory Match Game";
+            // 
+            // btnMode
+            // 
+            btnMode.Location = new Point(39, 308);
+            btnMode.Name = "btnMode";
+            btnMode.Size = new Size(97, 30);
+            btnMode.TabIndex = 22;
+            btnMode.Text = "Single Player";
+            btnMode.UseVisualStyleBackColor = true;
+            btnMode.Click += btnMode_Click;
+            // 
+            // btnRestart
+            // 
+            btnRestart.Location = new Point(171, 308);
+            btnRestart.Name = "btnRestart";
+            btnRestart.Size = new Size(75, 30);
+            btnRestart.TabIndex = 23;
+            btnRestart.Text = "Restart";
+            btnRestart.UseVisualStyleBackColor = true;
+            btnRestart.Click += btnRestart_Click;
+            // 
+            // lblScore
+            // 
+            lblScore.AutoSize = true;
+            lblScore.Font = new Font("Segoe UI", 10F);
+            lblScore.Location = new Point(39, 271);
+            lblScore.Name = "lblScore";
+            lblScore.Size = new Size(42, 19);
+            lblScore.TabIndex = 24;
+            lblScore.Text = "Score";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(295, 389);
+            Controls.Add(lblScore);
+            Controls.Add(btnRestart);
+            Controls.Add(btnMode);
+            Controls.Add(label1);
+            Controls.Add(button20);
+            Controls.Add(button19);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
+        private Label label1;
+        private Button btnMode;
+        private Button btnRestart;
+        private Label lblScore;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MemoryMatchGame
+{
+    public partial class Form1 : Form
+    {
+        List<Button> buttons = new List<Button>();
+        List<string> cardValues = new List<string>();
+        Button? firstCard = null;
+        Button? secondCard = null;
+        int turns = 0;
+        int playerTurn = 1;
+        int[] scores = new int[2];
+        bool multiplayer = false;
+        int matchedPairs = 0; // Counter for matched pairs
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            buttons = new List<Button>() { button1, button2, button3, button4, button5, button6, button7, button8, button9, button10,
+                                           button11, button12, button13, button14, button15, button16, button17, button18, button19, button20 };
+
+            // Initialize game logic variables
+            cardValues = GenerateCardValues();
+            AssignValuesToCards();
+            turns = 0;
+            scores = new int[] { 0, 0 }; // Player 1 and Player 2 score
+            matchedPairs = 0; // Reset matched pairs counter
+            UpdateScoreLabel();
+        }
+
+        private List<string> GenerateCardValues()
+        {
+            // Generate 10 pairs of values (e.g., "A", "B", ...)
+            List<string> values = new List<string> { "📌", "📌", "⭐", "⭐", "❤️", "❤️", "✈️", "✈️", "🎶", "🎶",
+                                                    "🍕", "🍕", "🎈", "🎈", "🏈", "🏈", "🍁", "🍁", "🥑", "🥑" };
+            Random random = new Random();
+            return values.OrderBy(x => random.Next()).ToList();
+        }
+
+        private void AssignValuesToCards()
+        {
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                buttons[i].Tag = new CardInfo { Value = cardValues[i], IsMatched = false }; // Assign the value and matched flag to the button's Tag
+                buttons[i].Text = ""; // Set the button text to empty (face down)
+                buttons[i].Enabled = true; // Enable the button for clicking
+                buttons[i].Font = new Font("Segoe UI Emoji", 12); // Set the font to support emojis
+                buttons[i].Click += new EventHandler(Card_Click);
+            }
+        }
+
+        private void Card_Click(object? sender, EventArgs e)
+        {
+            if (sender is not Button clickedButton) return;
+            if (clickedButton.Tag is not CardInfo cardInfo) return;
+
+            // If the clicked card is already matched, do nothing
+            if (cardInfo.IsMatched) return;
+
+            // If no card is selected, set the first card
+            if (firstCard == null)
+            {
+                firstCard = clickedButton;
+                firstCard.Text = cardInfo.Value; // Reveal the card's value
+                firstCard.Enabled = false; // Disable the first card to prevent double-clicking
+            }
+            // If a card is already selected, set the second card and check for match
+            else if (secondCard == null && clickedButton != firstCard)
+            {
+                secondCard = clickedButton;
+                secondCard.Text = cardInfo.Value; // Reveal the second card's value
+                secondCard.Enabled = false; // Disable the second card to prevent double-clicking
+
+                // Check for a match between the first and second card
+                CheckForMatch();
+            }
+        }
+
+        private void CheckForMatch()
+        {
+            if (firstCard == null || secondCard == null)
+                return; // Ensure both cards are selected
+
+            if (firstCard.Tag is not CardInfo firstCardInfo || secondCard.Tag is not CardInfo secondCardInfo)
+                return;
+
+            // If the selected cards match
+            if (firstCardInfo.Value == secondCardInfo.Value)
+            {
+                // It's a match, increment score and matched pairs
+                scores[playerTurn - 1]++;
+                matchedPairs++;
+
+                // Mark the cards as matched
+                firstCardInfo.IsMatched = true;
+                secondCardInfo.IsMatched = true;
+
+                // Clear the selection of first and second card immediately after matching
+                firstCard = null;
+                secondCard = null;
+
+                // Update the score label before checking if the game is over
+                UpdateScoreLabel();
+
+                // Check if the game is over (all pairs matched)
+                if (matchedPairs == cardValues.Count / 2)
+                {
+                    EndGame();
+                    return;
+                }
+            }
+            else
+            {
+                // Start the timer to flip the cards back if not a match
+                System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+                timer.Interval = 500; // Delay to show the second card before flipping back
+                timer.Tick += (s, e) =>
+                {
+                    if (firstCard != null && secondCard != null)
+                    {
+                        // Flip both cards face down if not a match
+                        firstCard.Text = "";
+                        secondCard.Text = "";
+
+                        // Re-enable the cards for future selections (since they weren't a match)
+                        firstCard.Enabled = true;
+                        secondCard.Enabled = true;
+                    }
+
+                    // Reset the selected cards
+                    firstCard = null;
+                    secondCard = null;
+
+                    // Stop the timer after it's done
+                    timer.Stop();
+                };
+                timer.Start(); // Start the timer to flip cards back
+            }
+
+            // Increment the number of turns
+            turns++;
+
+            // Switch player turns if in multiplayer mode
+            if (multiplayer)
+            {
+                playerTurn = (playerTurn == 1) ? 2 : 1;
+            }
+
+            // Update the score label
+            UpdateScoreLabel();
+        }
+
+        private void UpdateScoreLabel()
+        {
+            if (multiplayer)
+            {
+                lblScore.Text = $"Player {playerTurn}'s turn. Player 1: {scores[0]} | Player 2: {scores[1]}";
+            }
+            else
+            {
+                lblScore.Text = $"Turns: {turns}";
+            }
+
+            // Center the label
+            CenterLabel(lblScore);
+        }
+
+        private void CenterLabel(Label label)
+        {
+            // Calculate the new X position to center the label
+            int newX = (this.ClientSize.Width - label.Width) / 2;
+            label.Location = new Point(newX, label.Location.Y);
+        }
+
+        private void EndGame()
+        {
+            string message;
+            if (multiplayer)
+            {
+                if (scores[0] == scores[1])
+                {
+                    message = "It's a tie!";
+                }
+                else if (scores[0] > scores[1])
+                {
+                    message = "Player 1 wins!";
+                }
+                else
+                {
+                    message = "Player 2 wins!";
+                }
+            }
+            else
+            {
+                message = $"Game Over! It took {turns} turns.";
+            }
+
+            MessageBox.Show(message);
+        }
+
+        private void btnMode_Click(object sender, EventArgs e)
+        {
+            multiplayer = !multiplayer; // Switch between single and multiplayer
+            playerTurn = 1; // Reset to Player 1's turn
+            InitializeGame(); // Restart the game
+            btnMode.Text = multiplayer ? "Multiplayer" : "Single Player";
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            InitializeGame(); // Restart the game
+        }
+
+        // Class to store card information
+        private class CardInfo
+        {
+            public string Value { get; set; } = string.Empty;
+            public bool IsMatched { get; set; }
+        }
+    }
+}
